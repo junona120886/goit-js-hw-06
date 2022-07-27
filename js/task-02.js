f9b0bb71f0aca5b97fdb781d37c68419f9a59eab
Nataly Didenko <junona.logo@gmail.com>
2022-07-27 12:52:17 +0200

    task-02
    task-02 complited

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const refList = document.querySelector("#ingredients");
+refList.style.listStyle = "none";
+
+const createList = ingredients.map((value) => {
+  const item = document.createElement("li");
+  item.textContent = value;
+  item.className = "item";
+
+  return item;
+});
+
+refList.append(...createList);
