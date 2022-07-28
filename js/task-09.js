616619a81a5e8c03cf6adcbf61a304d6f11228ed
Nataly Didenko <junona.logo@gmail.com>
2022-07-28 23:46:57 +0200

    task-09
    task-09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+/* Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку 
+на button.change-color і виводить значення кольору в span.color.
+*/
+
+const bodyRef = document.querySelector("body");
+const colorSpanRef = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+const changeColorButtonHandler = () => {
+  const color = getRandomHexColor();
+  bodyRef.setAttribute("style", `background-color: ${color}`);
+  colorSpanRef.textContent = `${color}`;
+};
+
+changeColorButton.addEventListener("click", changeColorButtonHandler);
